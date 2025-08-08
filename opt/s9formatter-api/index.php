bd71dac2c51b41a647440d68907879ef57e9d368
Tristan Rivoallan <tristan@rivoallan.net>
2025-08-08 05:48:44 +0200

    fefe

--- opt/s9formatter-api/index.php
+++ opt/s9formatter-api/index.php
@@ -41,6 +41,7 @@ $text = filter_input(INPUT_GET, 'text');
 
 if ($text) {
     $text = urldecode($text);
+    $text = mb_convert_encoding($text, 'ISO-8859-1', 'UTF-8');
     $text = str_replace("<t>", "", $text);
     $text = str_replace("</t>", "", $text);
     $xml  = $parser->parse($text);
